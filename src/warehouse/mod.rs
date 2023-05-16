c25389ccf497a6ffad60b711659a30873966ce11
Xiaoming94 <mail@zyming.me>
2023-05-16 11:39:35 +0200

    add receive shipment functionality

--- src/warehouse/mod.rs
+++ src/warehouse/mod.rs
@@ -1,8 +1,17 @@
+use std::collections::HashMap;
+
+type ShipmentType = HashMap <String, u32>;
 pub fn create_warehouse(name: String) -> Warehouse
 {
-    Warehouse {m_name: String::from(name)}
+    Warehouse {m_name: String::from(name), m_shipment: HashMap::new()}
+}
+
+pub fn receive_shipment(inWarehouse: Warehouse ,shipment: ShipmentType) -> ()
+{
+
 }
 
 pub struct Warehouse {
-    m_name: String
+    m_name: String,
+    m_shipment: ShipmentType,
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,8 +1,23 @@
 use ikea_project_rust::warehouse;
+use std::collections::HashMap;
 
 #[test]
 fn create_warehouse()
 {
    let name: String = String::from("warehouse name");
    warehouse::create_warehouse(name); 
+}
+
+#[test]
+fn receive_shipment()
+{
+   let shipment = HashMap::from(
+      [
+         (String::from("stol"),1),
+         (String::from("table"),2),
+      ]
+   );
+
+   let m_warehouse = warehouse::create_warehouse(String::from("my warehouse"));
+   warehouse::receive_shipment(m_warehouse, shipment);
 }
